28c714688ae6993be5cf90ad6bdd12bb60a826ee
Edwing123 <egarciavalle2014@gmail.com>
2022-09-08 05:34:33 -0600

    fix: Vite aliases
    Setting relative paths for the aliases doesn't work, because in the end
    they are being interpreted as absolute paths, and due to this I was getting
    errors.
    
    The fix is setting absolute paths, instead of relatives.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import path from 'node:path'
+
 // https://astro.build/config
 export default defineConfig({
     vite: {
         resolve: {
             alias: {
-                '@components': './src/components/',
-                '@layouts': './src/layouts/',
+                '@components': path.resolve('.', 'src/components/'),
+                '@layouts': path.resolve('.', 'src/layouts/'),
             }
         }
     }
