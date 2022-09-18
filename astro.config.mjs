4476c9c48336c4fff7714f5907c89aae1d8c99cc
Edwing123 <egarciavalle2014@gmail.com>
2022-09-17 19:10:51 -0600

    chore: add Astro MDX integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import path from 'node:path'
+import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
+    integrations: [mdx()],
     vite: {
         resolve: {
             alias: {
