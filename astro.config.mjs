0555487e810d09ed2fe261d701c59b8ca201ac64
Edwing123 <egarciavalle2014@gmail.com>
2022-09-24 15:03:29 -0600

    chore: config syntax highlighter to use a light-theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,11 @@ import mdx from '@astrojs/mdx'
 // https://astro.build/config
 export default defineConfig({
     integrations: [mdx()],
+    markdown: {
+        shikiConfig: {
+            theme: 'min-light'
+        }
+    },
     vite: {
         resolve: {
             alias: {
